117467a9d7c8b846341eab363bdf005034247a4d
BenjaminDuchovny <126bduchovny@frhsd.com>
2024-02-21 13:13:31 -0500

    Hi alex

--- FirstGithubProject/src/Alex_Brosnan.java
+++ FirstGithubProject/src/Alex_Brosnan.java
@@ -2,6 +2,7 @@ public class Alex_Brosnan
 {
     public static void main(String[] args){
         System.out.println("Hi Alex B, from Joshua");
+
+    System.out.print("Hi Alex and hi Josh, from Ben");
     }
-    System.out.print("Hi Alex, from Ben");
 }
