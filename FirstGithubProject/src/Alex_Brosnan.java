f2aaf71b92292850ad0076c9db750cf9ddb31a26
AlexBrosnan <113187621+AlexBrosnan@users.noreply.github.com>
2024-02-21 13:37:42 -0500

    Revert "Merge pull request #3 from Stephengiammarino23/main"
    This reverts commit 7ea49457ab3fd8b8e938a09b6b2a2aa409c0d555, reversing
    changes made to 25675d94acf6dc4833435573fc486b23de542d6a.

--- FirstGithubProject/src/Alex_Brosnan.java
+++ FirstGithubProject/src/Alex_Brosnan.java
@@ -2,7 +2,7 @@ public class Alex_Brosnan
 {
     public static void main(String[] args){
         System.out.println("Hi Alex B, from Joshua");
+
     System.out.print("Hi Alex and hi Josh, from Ben");
-    System.out.print("Hi Alex, Hi Josh and Hi Ben from Stephen");
     }
 }
